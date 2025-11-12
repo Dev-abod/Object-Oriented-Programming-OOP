fccd30779c9415da8a1b6123c2c9ac1e21eeab5e
Ab3awd <Abdullahalwsabi56dev.des@gmail.com>
2025-11-12 21:15:53 +0300

    add All Management User

--- Bank Project By OOP/Bank Project By OOP/clsMainScreen.h
+++ Bank Project By OOP/Bank Project By OOP/clsMainScreen.h
@@ -8,6 +8,7 @@
 #include "clsUpdateClientScreen.h"
 #include "clsFindClientScreen.h"
 #include "clsTransactionsScreen.h"
+#include "clsManageUsersScreen.h"
 
 
 using namespace std;
@@ -84,7 +85,8 @@ private:
 
     static void _ShowManageUsersMenue()
     {
-        cout << "\nUsers Menue Will be here...\n";
+        // cout << "\nUsers Menue Will be here...\n";
+        clsManageUsersScreen::ShowManageUsersMenue();
 
     }
 
